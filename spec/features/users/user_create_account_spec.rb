fb4108dc4b473ce939557fa334dbb5bccf8e8794
Andy Wooten <andrewdwooten@icloud.com>
2017-02-14 18:14:15 -0700

    Add new, create, and show methods to UsersController.  Feature tests for new user currently all passing; testing standard creation, bad information case, and user existing already case.  Add current_user method to ApplicationController to prep for sessions.

---
+++ spec/features/users/user_create_account_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature 'user can create an account' do
+  scenario 'user submits information and this user is added to database' do
+
+    visit new_user_path
+
+    fill_in 'user[name]', with: 'test'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password'
+    expect { click_button 'Create Account'}.to change(User, :count).by(1)
+
+    expect(current_path).to eq user_path(User.last)
+    expect(page).to have_content("Welcome..#{User.last.name}..")
+  end
+
+  scenario 'user submits incomplete information and is redirected to the new user page' do
+
+    visit new_user_path
+
+    fill_in 'user[name]', with: ''
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: ''
+    fill_in 'user[password_confirmation]', with: ''
+
+    expect { click_button 'Create Account'}.to change(User, :count).by(0)
+
+    expect(current_path).to eq new_user_path
+  end
+
+  scenario 'user already exists and current user is redirected to the new user page' do
+    User.create(name: 'test', email: 'test@test.com', password_digest: 'password', password_confirmation: 'password')
+
+      visit new_user_path
+
+        fill_in 'user[name]', with: ''
+        fill_in 'user[email]', with: 'test@test.com'
+        fill_in 'user[password]', with: ''
+        fill_in 'user[password_confirmation]', with: ''
+
+        expect { click_button 'Create Account'}.to change(User, :count).by(0)
+
+        expect(current_path).to eq new_user_path
+  end
+end
