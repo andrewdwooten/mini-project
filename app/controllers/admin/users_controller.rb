2687bdb0bfdf2eba38e22d192ada8e25a1af178a
Andy Wooten <andrewdwooten@icloud.com>
2017-02-20 14:26:00 -0700

    Add ability for admin to create a new user.  Routes added to accomodate same.  View generated in admin/users reference same.  Methods added to Admin::UsersController.  Test passing.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,21 @@ class Admin::UsersController < Admin::BaseController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(name: params[:name],
+                    email: params[:email],
+                    password: params[:password])
+    if user.save
+      flash[:new_user] = 'User generated'
+      redirect_to admin_users_path
+    else
+      flash[:new_user_error] = 'User creation failed'
+      redirect_to new_admin_user_path
+    end
+  end
+
 end
